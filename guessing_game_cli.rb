b2c991ffac759a028ae8d30777852d0164b692c1
Diego Martinez <dmartinezm@gmail.com>
2019-10-05 15:57:50 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 def run_guessing_game
   comp_number= rand(6)+1 # random number from 1 to 6 included
-  puts "Please guess my number between 1 and 6:"
+  puts "Guess a number between 1 and 6:"
   usr_number= gets.chomp
   if comp_number == usr_number.to_s
     puts "You guessed the correct number!"
